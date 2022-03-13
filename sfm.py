793c31cbfb6e520daff8377a9720308f2bc63684
Colin Schultz <colin.r.schultz@gmail.com>
2022-03-12 19:33:55 -0800

    double_quaternion attempt

--- sfm.py
+++ sfm.py
@@ -5,7 +5,7 @@ from synthetic import generate_synthetic_points
 import matplotlib.pyplot as plt
 from utils import *
 
-def multibody_sfm(points,  K, iters=3000):
+def multibody_sfm(points,  K, iters=3000, verbose=False):
     """Compute 3D structure over multiple frames
 
     M = number of frames
@@ -20,22 +20,22 @@ def multibody_sfm(points,  K, iters=3000):
         structure - N x 3  3d world positions of rigid body
         projections - M x N x 3 2d pixel projections of N moints over M frames
     """
-    max_norm = np.max(np.linalg.norm(points[:, 1] - points[:, 0], axis=-1))
-    z_guess = K[0, 0] / max_norm
-    print("########Z_GUESS", z_guess)
 
     M, N, _ = points.shape
     X = tf.Variable(np.zeros((N, 3)), dtype=tf.float64)
-    angle = tf.Variable(np.zeros((M, 3)), dtype=tf.float64)
-    T = tf.Variable(np.tile([0, 0, z_guess], (M, 1)), dtype=tf.float64)
+    r = tf.convert_to_tensor([[0., 0., 0., 1.]])
+    t = tf.convert_to_tensor([[0., 0., 1.]])
+    double_q = tfg_transformation.dual_quaternion.from_rotation_translation(r, t)
+    RT = tf.Variable(tf.cast(tf.tile(double_q, (M, 1)), dtype=tf.float64))
+    
 
     K = tf.constant(K, dtype=tf.float64)
     points = tf.constant(points, dtype=tf.float64)
 
     @tf.function
     def project():
-        R = tfg_transformation.rotation_matrix_3d.from_euler(angle)
-        X_ = tfg_transformation.rotation_matrix_3d.rotate(tf.reshape(X, (1, N, 3)), tf.reshape(R, (M, 1, 3, 3)))
+        R, T = tfg_transformation.dual_quaternion.to_rotation_translation(RT)
+        X_ = tfg_transformation.quaternion.rotate(tf.reshape(X, (1, N, 3)), tf.reshape(R, (M, 1, 4)))
         X_ += tf.reshape(T, (M, 1, 3))
         pixels = X_ @ tf.transpose(K)
         pixels = pixels[:, :, :2] / pixels[:, :, 2:]
@@ -54,37 +54,24 @@ def multibody_sfm(points,  K, iters=3000):
         loss = res_loss + 0.01 * centroid_loss
         return loss
 
-    var = [X, angle, T]
+    var1 = [X]
+    var2 = [RT]
     opt = tf.keras.optimizers.Adam(0.01)
 
-    # features = get_features()
-    # print("features!", features.shape)
-    # np.save("features2objs0iters", features)
-
     for i in range(iters):
-        if i % 1000 == 0:
+        if verbose and i % 1000 == 0:
             l = loss()
             print(i, l.numpy())
-        opt.minimize(loss, var)
-
-
-    # features = get_features()
-    # print("features!", features.shape)
-    # np.save("features2objs10000iters", features)
+        if int(i / 100) % 2 == 0:
+            opt.minimize(loss, var1)
+        else:
+            opt.minimize(loss, var2)
+        # opt.minimize(loss, var2 + var1)
 
     res = residuals().numpy()
-    print("loss")
-    print(np.mean(np.linalg.norm(res, axis=-1), axis=0))
-
     return X.numpy(), project().numpy(), res
 
-if __name__ == "__main__":
-    M = 16
-    K = np.array([
-        [320, 0, 320],
-        [0, 320, 240],
-        [0, 0, 1]
-    ])
+def visualize(M, K):
     pts, p = generate_synthetic_points(K, M)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -93,7 +80,7 @@ if __name__ == "__main__":
     set_axes_equal(ax)
     plt.show()
 
-    pts2, p2, _ = multibody_sfm(p, K, iters=10000)
+    pts2, p2, _ = multibody_sfm(p, K, iters=3000)
     print(pts2)
 
     fig = plt.figure()
@@ -103,8 +90,6 @@ if __name__ == "__main__":
     set_axes_equal(ax)
     plt.show()
 
-
-    
     for i in range(M):
         plt.axis("equal")
         plt.xlim([0, 640])
@@ -112,3 +97,17 @@ if __name__ == "__main__":
         plt.scatter(p[i, :, 0], p[i, :, 1])
         plt.scatter(p2[i, :, 0], p2[i, :, 1])
         plt.show()
+
+
+if __name__ == "__main__":
+    M = 16
+    K = np.array([
+        [320, 0, 320],
+        [0, 320, 240],
+        [0, 0, 1]
+    ])
+    for i in range(10):
+        pts, p = generate_synthetic_points(K, M)
+        pts2, p2, res = multibody_sfm(p, K, iters=3000)
+        loss = np.mean(np.linalg.norm(res, axis=-1))
+        print(i+1, loss)
